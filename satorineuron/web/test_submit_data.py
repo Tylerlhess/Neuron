0ef1f011569b1dd2e1a1fa68d039255da5394646
Tyler Hess <tylerlhess@gmail.com>
2024-08-19 10:50:05 -0600

    adding in new reporting

--- satorineuron/web/test_submit_data.py
+++ satorineuron/web/test_submit_data.py
@@ -1,9 +1,12 @@
 import requests
+from flask import jsonify
 
 r = requests.session()
 result = r.get("http://45.8.148.105:61208/api/4/diskio")
-print(result.json())
+print(result.json()[2]["write_bytes_gauge"])
 data = {"data": result.json()[2]["write_bytes_gauge"]}
+print(data)
+data = jsonify(data)
 result = r.post("http://127.0.0.1:24622/submit_data", json=data)
 print(result.text)
 
