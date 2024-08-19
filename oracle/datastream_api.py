fbc499f0c6520fdc6c420d88d8c7721961468ccc
Tyler Hess <tylerlhess@gmail.com>
2024-08-19 11:18:57 -0600

    adding in new reporting

--- oracle/datastream_api.py
+++ oracle/datastream_api.py
@@ -17,6 +17,12 @@ logger = logging.basicConfig(
     ]
 )
 
+logging.debug("This is a DEBUG message")
+logging.info("This is an INFO message")
+logging.warning("This is a WARNING message")
+logging.error("This is an ERROR message")
+logging.critical("This is a CRITICAL message")
+
 DEBUG = 1
 
 def new_datastream(stream: Stream, port: int):
