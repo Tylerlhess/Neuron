b0bb7666b40faf9abffbde50e442135f03cb6428
Tyler Hess <tylerlhess@gmail.com>
2024-08-19 10:45:11 -0600

    adding in new reporting

--- satorineuron/web/test_submit_data.py
+++ satorineuron/web/test_submit_data.py
@@ -4,7 +4,7 @@ r = requests.session()
 result = r.get("http://45.8.148.105:61208/api/4/diskio")
 print(result.json())
 data = {"data": result.json()[2]["write_bytes_gauge"]}
-r.post("http://127.0.0.1:24622/submit_data", json=data)
+result = r.post("http://127.0.0.1:24622/submit_data", json=data)
 print(result.text)
 
 
