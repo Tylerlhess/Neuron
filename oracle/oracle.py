c01f6fac1838438770f7c486c3f8d1072e418a1c
Tyler Hess <tylerlhess@gmail.com>
2024-10-14 14:56:43 -0600

    trying to run directly in oracle.

--- oracle/oracle.py
+++ oracle/oracle.py
@@ -188,6 +188,16 @@ class Oracle:
         pass
 
     def handle_call(self, message: str="", return_port: int=None):
+        """ received message: {"topic": "{\"source\": \"satori\", \"author\": \"03ecc3fc46ce11cdda6deace2a52f1c700920c304377900dc6643b6aa2ada03ee4\", \"stream\": \"GLPG.USD.10mins\", \"target\": \"results.p\"}", "data": "29.275", "time": "2024-10-14 18:50:01.026029", "hash": "f2028c7376694599"}
+            received message: {"topic": "{\"source\": \"satori\", \"author\": \"03764db13d11dca864ef3e83e7b0257c3b6840f2bcf1fb80ad00e486032edca05d\", \"stream\": \"IMNN.USD.10mins\", \"target\": \"results.p\"}", "data": "1.04", "time": "2024-10-14 18:50:01.402422", "hash": "51f08d5078703654"}
+            received message: {"topic": "{\"source\": \"satori\", \"author\": \"03c062977efa58418f56cec6012ab47931dd0bd5b50a1969822f84316c4c944fcc\", \"stream\": \"HBB.USD.10mins\", \"target\": \"results.p\"}", "data": "29.98", "time": "2024-10-14 18:50:02.912412", "hash": "bf0242cff36e1838"}
+            received message: {"topic": "{\"source\": \"satori\", \"author\": \"0367a0c88402e602e11c412234ed25bfc5bbaa42b4463d155cf43f6d9e7962a7aa\", \"stream\": \"EOG.USD.10mins\", \"target\": \"results.p\"}", "data": "131.5431", "time": "2024-10-14 18:50:01.765019", "hash": "496583ac59266935"}
+            received message: {"topic": "{\"source\": \"satori\", \"author\": \"03b29caade8f73dc2460bb2dc3a32051bd277c5a48418a26f13a6de059dc91e76f\", \"stream\": \"GWAV.USD.10mins\", \"target\": \"results.p\"}", "data": "0.3986", "time": "2024-10-14 18:50:02.723712", "hash": "7a3e41363e7e1cd1"}
+            received message: {"topic": "{\"source\": \"satori\", \"author\": \"02bcbbb7a3a01335d9d97a39a99e611b1d255112eb6da80f990ca3a404a063cd86\", \"stream\": \"IBAC.USD.10mins\", \"target\": \"results.p\"}", "data": "10.02", "time": "2024-10-14 18:50:03.987507", "hash": "6ba151e3d0702d09"}
+            received message: {"topic": "{\"source\": \"satori\", \"author\": \"03507737a5b6c9eb2721e43d7bab15372e426ad8a76eb2988781fbe8547beccbe0\", \"stream\": \"GIB.USD.10mins\", \"target\": \"results.p\"}", "data": "115.3896", "time": "2024-10-14 18:50:09.544528", "hash": "5888b77feb99c7a9"}
+            received message: {"topic": "{\"source\": \"satori\", \"author\": \"022a410d73325a37cb10a17816423df2da140563502b8b37e304f55f4a2ca968b3\", \"stream\": \"CLPT.USD.10mins\", \"target\": \"results.p\"}", "data": "12.33", "time": "2024-10-14 18:50:08.077777", "hash": "2ae88a6411b2290d"}
+            received message: {"topic": "{\"source\": \"satori\", \"author\": \"033744da39677b98f4de7d846d2677cd07b1b9933f00e221c1dc628d1b54c0ee94\", \"stream\": \"AISP.USD.10mins\", \"target\": \"results.p\"}", "data": "2.26", "time": "2024-10-14 18:50:01.704882", "hash": "fbc47780bf98d70c"}
+        """
         try:
             data = json.loads(message)
             try:
@@ -195,10 +205,11 @@ class Oracle:
                 if return_port is None:
                     stream_port = max((self.streams[port] for port in self.streams))
                     stream_port += 1
-                    stream = Stream(StreamId(**data))
+                    stream = Stream(StreamId(topic["source"], topic["author"], topic["stream"], topic["target"]))
                     print(f"attempting to start recording a datastream {topic} on port {stream_port}")
                     threading.Thread(target=datastream_api.new_datastream, args=(stream, stream_port)).start()
                     return_port = stream_port
+                    self.streams[stream_port] = topic
                 Oracle.submit_stream_data(data, return_port)
 
                 returnable = True
@@ -220,7 +231,7 @@ class Oracle:
     
     def run(self, port):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.bind(('localhost', 24621))
+        server_socket.bind(('localhost', port))
         server_socket.listen(25)
         while True:
             message, client_socket = server_socket.accept()
@@ -230,5 +241,23 @@ class Oracle:
     def accept_stream(self, port):
         self.streams[str(port)] = requests.get(f"http://127.0.0.1:{port}/topic")
         return True
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            oracle = Oracle()
+            threading.Thread(target=Oracle.run, args=(24621)).start()
+                    
+            break
+        except ConnectionError as e:
+            # try again...
+            logging.error(f'ConnectionError in app startup: {e}', color='red')
+            time.sleep(30)
+        # except RemoteDisconnected as e:
+        except Exception as e:
+            # try again...
+            logging.error(f'Exception in app startup: {e}', color='red')
+            time.sleep(30)
 
 
